80997f2f2991d578d9da139201bd25868308674d
Pat Cleaver <pat.cleaver.bass@gmail.com>
2021-01-30 14:27:05 +0100

    added footstep sounds to FMOD, and tagged the correct frames in the animations. The sounds themselves need work, but triggering behaviour is correct

---
+++ GGJAM_21/Assets/Scripts/Audio/Audio_Player_Sounds.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_Player_Sounds : MonoBehaviour
+{
+
+    void PlayFootstepSound ()
+    {
+        FMOD.Studio.EventInstance Footstep = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/Footstep");
+        Footstep.start();
+        Footstep.release();
+    }
+}
