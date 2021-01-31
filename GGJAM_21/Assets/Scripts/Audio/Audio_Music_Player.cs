8565aba1bb77f91a15843b69a92b62b44d7af359
Pat Cleaver <pat.cleaver.bass@gmail.com>
2021-01-31 01:30:31 +0100

    Music Player go beep boop (there's a MusicPlayer prefab that needs to be placed in whatever scenes we end up using)

--- GGJAM_21/Assets/Scripts/Audio/Audio_Music_Player.cs
+++ GGJAM_21/Assets/Scripts/Audio/Audio_Music_Player.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Audio_Music_Player : MonoBehaviour
 {
-    [FMODUnity.ParamRef]
-    [SerializeField]
-    private string ParameterName;
     FMOD.Studio.EventInstance musicInstance;
+    public float glitches;
+    public float bleeps;
+    public float beat;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +15,22 @@ public class Audio_Music_Player : MonoBehaviour
         musicInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Music");
         musicInstance.start();
         musicInstance.release();
+    }
+
+    private void Update()
+    {
+        checkTheVibe();
+    }
+
+    void checkTheVibe ()
+    {
+        //this needs hooking up to set values for the parameter floats but for now:
+        //glitches = 1f;
+        //bleeps = 1f;
+        //beat = 1f;
+        musicInstance.setParameterByName("GlitchHat", glitches, false);
+        musicInstance.setParameterByName("Bleeps", bleeps, false);
+        musicInstance.setParameterByName("Beat", beat, false);
     }
 
     private void OnDestroy()
