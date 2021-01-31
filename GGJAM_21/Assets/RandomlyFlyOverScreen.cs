067c4101d61f7278a9bffa97b2b683322ca1ed8c
Kleissen,H.D.J. (Hilko) <h.d.j.kleissen@students.uu.nl>
2021-01-31 04:55:03 +0100

    Redid one tile collider, went back to square collider for player, added easter egg on title screen

---
+++ GGJAM_21/Assets/RandomlyFlyOverScreen.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomlyFlyOverScreen : MonoBehaviour
+{
+    Rigidbody2D rigidBody2D;
+    float rotationSpeed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        StartNewRun();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 1), rotationSpeed * Time.deltaTime);
+    }
+
+    void StartNewRun()
+    {
+        transform.position = new Vector3(Random.Range(-40, 40), Random.Range(-20, 50), transform.position.z);
+        rigidBody2D.velocity = new Vector2(-transform.position.x / 40 * 25, Random.Range(-15, 15));
+        rotationSpeed = Random.Range(-Mathf.Abs(transform.position.x / 50 * 35), Mathf.Abs(transform.position.x / 500 * 35));
+        StartCoroutine(CoroutineHelper.DelaySeconds(() => StartNewRun(), Random.Range(8, 20)));
+    }
+}
